adf2328d40d48ce4c573a6d5861a1b5d27b7b999
Josh Wilson <joshmw@stanford.edu>
2022-07-11 17:17:28 -0700

    imported analysis from synthNoise

--- avgModelNoise.m
+++ avgModelNoise.m
@@ -22,17 +22,75 @@ load(data1); cleanRoisA = rois;
 load(data2); cleanRoisB = rois;
 
 
-%% Subtract mean model and plot noise %%
-for roi = 1:length(cleanRoisA)
-    for voxel = 1:length(cleanRoisA(roi).vox.r2)
-        if (cleanRoisA(roi).vox.r2(voxel) > .5 & cleanRoisB(roi).vox.r2(voxel) > .5)
-            figure(roi), hold on, scatter(zscore(cleanRoisA(roi).vox.tSeries(:,voxel)),zscore(cleanRoisA(roi).vox.tSeries(:,voxel))-zscore(cleanRoisB(roi).vox.tSeries(:,voxel)),1,'filled','k');
-            xlabel('model time series % signal'),ylabel('residual (mean model - true)');
-        end
+
+
+
+
+% parse arguments 
+startparams(1) = 1; startparams(2) = 0;
+opts = optimset('display','off','maxIter',1000000,'MaxFunEvals',1000000);
+minsearch = [0 -inf]; maxsearch = [inf inf];
+
+%% iterate through fixed multiplicative noise scales, just varying additive noise, and find the additive noise magnitudes and likelihoods
+% multiplicative scale to iterate through
+multiplicativeScales = -.5:.5:.5;
+keyboard
+% go through voxels 
+for voxel = 1:cleanRoisA(1).n
+    iteration = 1;
+    for multiplicativeScale = multiplicativeScales;
+     [params, resnorm, residual, exitflag, output, lambda, jacobian] = ... 
+         lsqnonlin(@fitNoiseParameters,startparams(1),minsearch(1),maxsearch(1),opts,cleanRoisA,cleanRoisB,voxel,0,1,multiplicativeScale);
+
+    additiveFit.parameters{voxel}{iteration} = params; additiveFit.resnorms{voxel}{iteration} = resnorm; additiveFit.residual{voxel}{iteration} = residual(1)-1000000; additiveFit.exitflag{voxel}{iteration} = exitflag; additiveFit.output{voxel}{iteration} = output; additiveFit.lambda{voxel}{iteration} = lambda; additiveFit.jacobian{voxel}{iteration} = jacobian;
+    iteration = iteration+1;
     end
 end
+keyboard
+figure, hold on, averageResiduals = 0;
+for voxel = 1:cleanRoisA(1).n
+    plot(multiplicativeScales,cell2mat(additiveFit.residual{voxel}{1}),'lineWidth',1,'color',[.8 .8 .8])
+    averageResiduals = averageResiduals + cell2mat(additiveFit.residual{voxel});
+end
+plot(multiplicativeScales,averageResiduals/cleanRoisA(1).n,'lineWidth',3,'color',[0 0 0])
+xlabel('Multiplicative Noise Scale'),ylabel('Log Likelihood'),title('Log Likelihoods: Fit additive noise with varying multiplicative scaling');
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% fitNoiseParameters %%
+%%%%%%%%%%%%%%%%%%%%%%%%
+function logLikelihood = fitNoiseParameters(params,cleanRoisA,cleanRoisB,voxel,fitAll,additiveFit,multiplicativeScale)
+
+% Parsing parameters and get tseries length
+if fitAll; additiveNoiseStd = params(1); multiplicativeNoiseScale = params(2); end
+if additiveFit; additiveNoiseStd = params(1); multiplicativeNoiseScale = multiplicativeScale; end
+timeSeriesLength = cleanRoisA(1).nFrames;
+
+% get residual T series as mean model Tseries - measured single Tseries
+residualTSeries = cleanRoisA(1).vox.tSeries(:,voxel) - cleanRoisB(1).vox.tSeries(:,voxel);
+
+% calculate Std at every time point as a function of the model BOLD activity. Second term is a variance term, so take the sqrt (poisson mean-variance relationship)
+noiseStdTimeSeries = additiveNoiseStd*ones(1,timeSeriesLength) + multiplicativeNoiseScale*cleanRoisA(1).vox.pRFtSeries(:,voxel);
+
+% compute the likelihood of each residual time point %
+logLikelihood = sum(-log(normpdf(residualTSeries,0,noiseStdTimeSeries)))+1000000;
 
 
